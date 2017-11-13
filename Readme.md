16d3b6049757bcb6f8b11b8b133ecc3095bb865e
Cornerpirate <cornerpirate@gmail.com>
2017-11-13 17:56:44 -0500

    Initial commit, before Readme gets sorted

---
+++ Readme.md
@@ -0,0 +1,25 @@
+== Overview
+
+== Install Pre-Requisites
+
+We need "pyautogui" which has an installation guide for various platforms here:
+
+http://pyautogui.readthedocs.io/en/latest/install.html
+
+For Kali Linux, the required commands were:
+
+pip install python3-xlib
+apt-get install scrot python3-dev
+pip install pyautogui
+
+Any problems installing pyautogui then refer to their documentation for your OS.
+
+Additionally we need this for the sexy progress update bar:
+
+pip install tqdm
+
+Finally, this for sound:
+
+pip install playsound
+
+
---
+++ rdpupload.py
@@ -0,0 +1,86 @@
+# Created by CornerPirate on 10/11/2017
+# This zips any file and then base64 encodes it.
+# It then delays for 10 seconds so you can get your mouse pointer in place.
+# This will then  type the encoded characters wherever your pointer has focus.
+# Useful when uploading files to RDP/VNC if he system does not allow 
+# Drag and drop of files or paste.
+from pyautogui import press, typewrite, hotkey
+from playsound import playsound
+from tqdm import tqdm
+from multiprocessing import Process
+import sys
+import os
+import zipfile
+import base64
+import time
+import argparse
+
+
+####################### Library Functions #########################
+
+# Splits a string (s) into chunks of size (n)
+def nsplit(s, n):
+    return [s[k:k+n] for k in xrange(0, len(s), n)]
+
+# Plays the typewriter sound
+def playTypewriter():
+    while True:
+        playsound("typewriter.mp3")
+
+###################################################################
+
+# Defining the arguments and parsing the args.
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="file that you want to upload")
+parser.add_argument("-d", "--delay", type=int, help="time in seconds to delay before pasting (default is 10)", default=10)
+parser.add_argument("-s", "--sound", help="plays a typewriter sound until upload is finished", action='store_true')
+args = parser.parse_args()
+
+# Zip the intended file, might save some space if what you are uploading 
+# Can save some space by zipping.
+zfile = args.file + ".zip"
+
+zipf = zipfile.ZipFile(zfile, 'w')
+zipf.write(args.file)
+zipf.close()
+
+# Tell the user
+print "[*] Zipped up: " + zfile
+print "[*] Base64 encoding zipped file "
+
+# Base64 encode the zipped file
+b64 = base64.encodestring(open(zfile,'rb').read())
+
+# Cleanup the zipped file
+print "[*] Deleting the zip that was made"
+os.remove(zfile)
+
+# Sleep to give the user time to get their mouse pointer in place
+print "[*] Sleeping for " + str(args.delay) + " seconds. Get your mouse pointer into Notepad within your RDP Session"
+time.sleep(args.delay)
+
+# Tested the time for uploading the entire file versus splitting into chunks and saw no difference really.
+# So might as well split so we can get a progress update.
+splits = nsplit(b64, 256)
+max = len(splits) # number of slices of size 256
+
+# Lets be classy eh? Let you know when it is finished.
+p = None
+if args.sound:
+    p = Process(target=playTypewriter)
+    p.start()
+
+# Do the typing and use tqdm to display a progress bar
+for split in tqdm(splits):
+	typewrite(split)
+
+# We got through it. Kill the sounds
+if args.sound and p !=None:
+    p.terminate()
+
+# Close down routine and tell the user how to retrieve their file
+print "[*] Upload complete"
+print "[*] Save the file in your text editor on the windows system"
+print "[*] Use 'certutil' to base64 decode to retrieve the intended file"
+print "[*] certutil -decode <input_file> <output_file>"
+print "[*] The output file needs a .zip extension. You can then unpack that as Windows has a zip util"
